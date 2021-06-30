e782cc635eacc979003fe04656706e51cb56c2f4
visargsoneji <visargbs22@gmail.com>
2021-07-01 01:11:00 +0530

    main files

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const client = require('@mailchimp/mailchimp_marketing');
+const keys = require("./keys.json")
+
+const app = express();
+const PORT = 5500;
+
+client.setConfig({
+    apiKey: keys.apiKey,
+    server: 'us6',
+});
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/sign-up", (req, res) => {
+    let data = [
+        {
+            email_address: req.body.email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: req.body.firstName,
+                LNAME: req.body.lastName
+            }
+        }
+    ]
+
+    const run = async () => {
+        const response = await client.lists.batchListMembers(keys.listID, {
+            members: data,
+        });
+        res.send(response);
+    };
+
+    run();
+});
+
+app.get('/list', (req, res) => {
+    let baseUrl = "https://us6.api.mailchimp.com/3.0/lists";
+    axios.get(baseUrl, {
+        auth: {
+            username: 'BigO',
+            password: keys.apiKey
+        }
+    }).then((response) => res.send(response.data))
+        .catch((err) => res.send(err));
+})
+
+app.listen(PORT, () => console.log("Server running on PORT: " + PORT));
